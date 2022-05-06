2a301344cbe9c4a4a8ee5268a002a7bf5d6b9d6d
joey57 <njorogejoey5@gmail.com>
2022-05-06 14:50:01 +0300

    create pitch class

--- app/models.py
+++ app/models.py
@@ -21,9 +21,22 @@ class User(db.Model):
     def verify_password(self,password):
       return check_password_hash(self.pass_secure,password)   
 
-
-    
-
     def __repr__(self):
         return f'User {self.username}'
+
+class Pitch(db.Model):
+  __tablename__ = 'pitches'
+
+  id = db.Column(db.Integer,primary_key = True)
+  owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable =False)
+  description = db.Column(db.String(255),index = True)
+  title = db.Column(db.String())
+  category = db.Column(db.String(255),nullable=False)
+
+
+  def __repr__(self):
+    return f'Pitch{self.description}'
+
+
+
 
