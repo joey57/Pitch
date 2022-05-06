01ca8f7b0d3bdca23d6252919d0c49b2767a04c0
joey57 <njorogejoey5@gmail.com>
2022-05-06 15:42:49 +0300

    create home page

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template, request, redirect,url_for,abort,flash
 from . import main
 from flask_login import login_required, current_user
 from ..models import Pitch, User, Comment, Upvote, Downvote
+from flask.views import View, MethodView
+from .. import db
 
 # views
 @main.route('/')
@@ -9,5 +11,12 @@ def index():
   '''
   view root page function that returns index page
   '''
-  return render_template('index.html')
+  title ='Home'
+  pitch =Pitch.query.filter_by().all()
+  pickuplines = Pitch.query.filter_by(category="pickuplines")
+  interviewpitch = Pitch.query.filter_by(category = "interviewpitch")
+  promotionpitch = Pitch.query.filter_by(category = "promotionpitch")
+  productpitch = Pitch.query.filter_by(category = "productpitch")
+
+  return render_template('home.html', title=title, pitch = pitch,pickuplines=pickuplines, interviewpitch= interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch)
   
