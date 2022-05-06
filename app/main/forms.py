f287c215cf8d537057e892c9c3a587ce8bead8ce
joey57 <njorogejoey5@gmail.com>
2022-05-06 17:46:44 +0300

    edit profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,12 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, RadioField
 from wtforms.validators import DataRequired
 
+
 class PitchForm(FlaskForm):
   title =StringField('Title' , validators=[DataRequired()])
   description = TextAreaField('What would you like to pitch?', validator=[DataRequired()])
   category = RadioField('Label', choices=[ ('promotionpitch','promotionpitch'), ('interviewpitch','interviewpitch'),('pickuplines','pickuplines'),('productpitch','productpitch')],validators=[DataRequired()])
   submit = SubmitField('submit')
-  
+
+class UpdateProfile(FlaskForm):
+  bio = TextAreaField('Tell us about you', validators=[DataRequired()])
+  submit = SubmitField('Submit')  
+    
 class CommentForm(FlaskForm):
 	description = TextAreaField('Add comment',validators=[DataRequired()])
 	submit = SubmitField()
--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from flask_login import login_required, current_user
 from ..models import Pitch, User, Comment, Upvote, Downvote
 from flask.views import View, MethodView
 from .. import db
-from .forms import PitchForm, CommentForm, UpvoteForm
+from .forms import PitchForm, CommentForm, UpvoteForm, UpdateProfile
 
 # views
 @main.route('/')
@@ -38,5 +38,33 @@ def new_pitch():
         
         
         return redirect(url_for('main.index'))
-    return render_template('pitches.html',form=form)  
+    return render_template('pitches.html',form=form) 
+
+@main.route('/user/<uname>')
+def profile(uname):
+  user = User.query.filter_by(username = uname).first()
+
+  if user is None:
+    abort(404)
+  return render_template("profile/profile.html",user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET', 'POST'])
+@login_required
+def update_profile(uname):
+  user = User.query.filter_by(username =uname).first()
+  if user is None:
+    abort(404)
+
+  form = UpdateProfile()
+  if form.validate_on_submit():
+    user.bio = form.bio.data
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(url_for('.profile',uname=user.username))
+
+  return render_template('profile/update.html',form = form)    
+
+
   
