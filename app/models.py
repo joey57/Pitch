88295a846d797bcae0371db72f504bd8b098c2e7
joey57 <njorogejoey5@gmail.com>
2022-05-06 14:27:01 +0300

    initialize flask login

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from re import I
 from . import db
+from flask_login import UserMixin
 
 class User(db.Model):
     __tablename__ = 'users'
-    
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
