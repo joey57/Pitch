713ec1baa5cdd19c4fdc214408cc80cae7c919cd
joey57 <njorogejoey5@gmail.com>
2022-05-06 17:20:48 +0300

    edit login route

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,10 +5,11 @@ from wtforms.validators import DataRequired,Email,EqualTo
 from ..models import User
 
 class RegistrationForm(FlaskForm):
-  email = StringField('Your Email Address', validators=[DataRequired(),Email()])
-  username = StringField('Enter your username', validators= [DataRequired()])
-  password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
-  submit = SubmitField('Sign up')
+  email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+  username = StringField('Enter your username',validators=[DataRequired()])
+  password = PasswordField('Password',validators=[DataRequired(),EqualTo('password_confirm',message = 'Passwords must match')])
+  password_confirm = PasswordField('Confirm Passwords',validators = [DataRequired()])
+  submit = SubmitField('Sign Up')
 
   def validate_email(self,data_field):
     if User.query.filter_by(email=data_field.data).first():
--- app/auth/views.py
+++ app/auth/views.py
@@ -5,16 +5,17 @@ from .forms import RegistrationForm, LoginForm
 from ..import db
 from . import auth
 
-@auth.route('/login')
+@auth.route('/login', methods =['GET','POST'])
 def login():
   login_form = LoginForm()
   if login_form.validate_on_submit():
-    user = User.query.filter.by(email = login_form.email.data).first()
+    user = User.query.filter_by(email = login_form.email.data).first()
     if user is not None and user.verify_password(login_form.password.data):
       login_user(user,login_form.remember.data)
       return redirect(request.args.get('next') or url_for('main.index'))
 
     flash('Invalid username or password')
+
   title = "Pitch login"    
   return render_template('auth/login.html',login_form = login_form,title=title)
 
