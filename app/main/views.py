fabe86116245fb5595556cb2a77051551f052672
joey57 <njorogejoey5@gmail.com>
2022-05-06 18:47:47 +0300

    home page change

--- app/main/views.py
+++ app/main/views.py
@@ -7,14 +7,23 @@ from .. import db
 from .forms import PitchForm, CommentForm, UpvoteForm, UpdateProfile
 
 # views
-@main.route('/')
+@main.route('/', methods = ['GET','POST'])
 def index():
-    pitches = Pitch.query.all()
-    job = Pitch.query.filter_by(category = 'Job').all() 
-    event = Pitch.query.filter_by(category = 'Events').all()
-    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    pitch = Pitch.query.filter_by().first()
+    title = 'Home'
+    pickuplines = Pitch.query.filter_by(category="pickuplines")
+    interviewpitch = Pitch.query.filter_by(category = "interviewpitch")
+    promotionpitch = Pitch.query.filter_by(category = "promotionpitch")
+    productpitch = Pitch.query.filter_by(category = "productpitch")
+
+    # upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
     
-    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
+
+    return render_template('home.html', title=title, pitch = pitch, pickuplines = pickuplines, interviewpitch= interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch)
 
 @main.route('/pitches/new/', methods = ['GET','POST'])
 @login_required
